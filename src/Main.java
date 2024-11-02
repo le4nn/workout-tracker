e00c8f38f9aee2a51f7f22c1bc9afcf838fb9a59
DIAS <alibekdias36@gmail.com>
2024-11-02 13:29:22 +0500

    create repository

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        WorkoutController controller = new WorkoutController();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+
+        server.createContext("/workouts", new WorkoutHandler(controller));
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Сервер запущен на http://localhost:8081/workouts");
+    }
+}
