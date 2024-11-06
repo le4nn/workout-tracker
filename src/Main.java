a92dac38bd2de6d46d81c9c4f0e2c597d44a916a
DIAS <alibekdias36@gmail.com>
2024-11-06 23:07:16 +0500

    добавил прогресс трекер

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        WorkoutController controller = new WorkoutController(new ArrayList<>());
+        ProgressTracker progressTracker = new ProgressTracker();
+        WorkoutController controller = new WorkoutController(new ArrayList<>(), progressTracker);
 
-        Observer.StatsModule statsModule = new Observer.StatsModule();
+        Observer.StatsModule statsModule = new Observer.StatsModule(progressTracker);
         controller.registerObserver(statsModule);
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
